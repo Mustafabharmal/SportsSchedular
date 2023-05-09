77c94d3be279e228f0d9166f651c15273b2787cb
Mustafa Bharmal <92981596+Mustafabharmal@users.noreply.github.com>
2023-05-09 15:01:02 +0530

    Update sport.js

--- models/sport.js
+++ models/sport.js
@@ -2,18 +2,11 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Sport extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       Sport.hasMany(models.Session, { foreignKey: "sportId" });
     }
-
     static addSport(sportname) {
       try {
-        // console.log(userID);
         return this.create({
           sportname,
         });
